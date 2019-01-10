dded920eb2dd81b4cd7dc1c5752626d788e55baf
hines-r <39021120+hines-r@users.noreply.github.com>
2019-01-10 14:47:25 -0700

    Implemented show message dialog

---
+++ MahAppsDialog/ViewModels/ButtonViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using MahApps.Metro.Controls.Dialogs;
+using MahAppsDialog.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MahAppsDialog.ViewModels
+{
+    public class ButtonViewModel : Screen
+    {
+        private IDialogCoordinator dialogCoordinator;
+
+        public ButtonViewModel(IDialogCoordinator instance)
+        {
+            dialogCoordinator = instance;
+        }
+
+        public ICommand ShowDialogCommand => new RelayCommand(ExecuteShowDialog);
+
+        public async void ExecuteShowDialog(object o)
+        {
+            await dialogCoordinator.ShowMessageAsync(this, "HEADER", "MESSAGE");
+        }
+    }
+}
--- MahAppsDialog/ViewModels/ShellViewModel.cs
+++ MahAppsDialog/ViewModels/ShellViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using Caliburn.Micro;
+using MahApps.Metro.Controls.Dialogs;
+using MahAppsDialog.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MahAppsDialog.ViewModels
 {
-    public class ShellViewModel : Screen
+    public class ShellViewModel : Conductor<Screen>
     {
-
+        public ShellViewModel()
+        {
+            ActivateItem(new ButtonViewModel(DialogCoordinator.Instance));
+        }
     }
 }
